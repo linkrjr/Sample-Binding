9d355bd88611056f33b611ab03e0f98a852be916
Ronaldo Gomes <ronaldo.junior@gmail.com>
2016-04-28 17:27:01 +1000

    playing with binds

--- Sample-Binding/ViewController.swift
+++ Sample-Binding/ViewController.swift
@@ -8,18 +8,105 @@
 
 import UIKit
 
+extension UILabel {
+    
+    struct AssociatedKeys {
+        static var BondKey:UInt8 = 0
+    }
+
+    var textBond: Bond<String> {
+        
+        if let b: AnyObject = objc_getAssociatedObject(self, &AssociatedKeys.BondKey) {
+            return b as! Bond<String>
+        } else {
+            let b = Bond<String>() { [unowned self] value in
+                self.text = value
+            }
+            objc_setAssociatedObject(self, &AssociatedKeys.BondKey, b, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+            return b
+        }
+        
+    }
+
+}
+
+class BondBox<T> {
+    weak var bond: Bond<T>?
+    init(_ b:Bond<T>) { bond = b }
+}
+
+class Bond<T> {
+    
+    typealias Listener = T -> Void
+    
+    var listener: Listener
+    
+    init(_ listener: Listener) {
+        self.listener = listener
+    }
+    
+    func bind(dynamic: Dynamic<T>) {
+        dynamic.bonds.append(self)
+    }
+    
+}
+
+class Dynamic<T> {
+    
+    var bonds:[Bond<T>] = []
+    
+    var value: T {
+        didSet {
+            bonds.map { (bond) -> Void in
+                bond.listener(value)
+            }
+        }
+    }
+    
+    init(_ v:T) {
+        self.value = v
+    }
+    
+    func map<U>(transform: T -> U) -> Dynamic<U> {
+        return Dynamic<U>(transform(value))
+    }
+    
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak private var nameLabel:UILabel!
+    @IBOutlet weak private var nameLabel2:UILabel!
+    @IBOutlet weak private var textField:UITextField!
+
+    var name: Dynamic<String> = Dynamic("Ronaldo")
+    
+    var tapCount: Dynamic<Int> = Dynamic(0)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.nameLabel.textBond.bind(self.name)
+        
+        self.nameLabel2.textBond.bind(self.tapCount.map({ (v) -> String in
+            return "\(v)"
+        }))
+        
+        self.textField.addTarget(self, action: #selector(ViewController.textChanged(_:)), forControlEvents: .EditingChanged)
+        
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(ViewController.tapped(_:))))
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tapped(sender:AnyObject) {
+        
+        self.tapCount.value += 1
+        
     }
-
+    
+    func textChanged(sender:AnyObject) {
+        self.name.value = self.textField.text!
+    }
 
 }
 
